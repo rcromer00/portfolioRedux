bb66233787ed4a91475c7d319ad935f77d1ec036
rcromer00 <cromerro@msu.edu>
2021-01-15 18:08:20 -0500

    server

---
+++ portfolioApp.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const htmlRoutes = require("./routes/html-routes");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT)
+});
